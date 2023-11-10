c9180fa30b7e2b79d8ae25a7c160c0b0b1e16d05
Ritesh Agarwal <ju7stritesh@gmail.com>
2023-11-09 19:09:15 -0500

    Update Constants.py
    Remove information

--- CalculatePose/Constants.py
+++ CalculatePose/Constants.py
@@ -1,18 +1,3 @@
 import os
 import logging
 logger = logging.getLogger(__name__)
-
-
-# Application Globals
-# AWS_ACCESS_KEY = 'AKIAUOTQKFBBUJE644UT'
-# AWS_SECRET_KEY = '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'
-# IMAGES_BUCKET_NAME = 'sentry-images-bucket'
-# SQS_QUEUE_NAME = os.getenv('SQS_Queue', "No SQS_Queue set")
-# PEOPLE_RESULT_HANDLER_LAMBDA_NAME = os.getenv('RESULT_LAMBDA', "No RESULT_LAMBDA set")
-# TWILIO_LAMBDA_NAME = os.getenv('call_twilio', "No call_twilio set")
-# TWILIO_IMAGE_BUCKET_NAME = 'sentry-twilio-images'
-# REGION = "us-west-2"
-# CAMERA_CODE = 'camera_id'
-# REC_KEY_CODE = 'received_key'
-
-ARN_URL = 'arn:aws:states:us-west-2:306251966531:activity:PoseEstimation'
